7841547b968f5a31e9b2859d340a18dbcad8e95e
Kartik Jayram Jondhalekar <88024268+KartikJondhalekar@users.noreply.github.com>
2026-04-06 21:11:38 -0400

    Add: longest-consecutive-sequence - submission-9

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-9.py
@@ -0,0 +1,28 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        heap = []
+        res = 1
+        curr_max = 1 
+
+        if not nums:
+            return 0
+
+        for num in nums:
+            heapq.heappush(heap, num)
+
+        prev = heapq.heappop(heap)
+
+        while heap:
+            curr = heapq.heappop(heap)
+            diff = abs(curr - prev)
+            if diff == 0:                
+                continue
+            if diff == 1 or diff == 0:
+                curr_max += 1
+            else:
+                curr_max = 1
+            res = max(res, curr_max)
+            prev = curr
+        
+
+        return res 
