b94e0b59abc685650412afe43e6c909cb0d90906
Kartik Jayram Jondhalekar <88024268+KartikJondhalekar@users.noreply.github.com>
2026-04-06 21:36:03 -0400

    Add: longest-consecutive-sequence - submission-11

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-11.py
@@ -0,0 +1,46 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # Time: O(n log n)
+        # Space: O(n)
+        # heap = []
+        # res = 1
+        # curr_max = 1 
+
+        # if not nums:
+        #     return 0
+
+        # for num in nums:
+        #     heapq.heappush(heap, num)
+
+        # prev = heapq.heappop(heap)
+
+        # while heap:
+        #     curr = heapq.heappop(heap)
+        #     diff = abs(curr - prev)
+        #     if diff == 0:                
+        #         continue
+        #     if diff == 1:
+        #         curr_max += 1
+        #     else:
+        #         curr_max = 1
+        #     res = max(res, curr_max)
+        #     prev = curr
+        
+        # return res 
+
+        seq = set(nums)
+        curr_max = res = 0
+
+        for num in nums:
+            if (num - 1) not in seq:
+                curr_max = 1
+                num += 1
+                while (num) in seq:
+                    curr_max += 1
+                    num += 1
+                res = max(res, curr_max)
+
+        return res
+
+
+
