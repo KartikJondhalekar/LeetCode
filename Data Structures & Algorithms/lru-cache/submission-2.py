3391d66639d42cbe4e818964e4a053e03487d2c6
Kartik Jayram Jondhalekar <88024268+KartikJondhalekar@users.noreply.github.com>
2026-05-03 11:55:49 -0400

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.py
@@ -0,0 +1,74 @@
+class LRUCache:
+    # Using deque
+    # def __init__(self, capacity: int):
+    #     self.cache = dict()
+    #     self.dq = deque()
+    #     self.capacity = capacity
+        
+    # def get(self, key: int) -> int:
+    #     if key not in self.cache:
+    #         return -1
+
+    #     self.dq.remove(key)
+    #     self.dq.append(key)
+
+    #     return self.cache[key]
+
+    # def put(self, key: int, value: int) -> None:
+    #     if key in self.cache:
+    #         self.dq.remove(key)
+    #     elif len(self.dq) == self.capacity:
+    #         lru = self.dq.popleft()
+    #         del self.cache[lru]
+
+    #     self.dq.append(key)
+    #     self.cache[key] = value
+
+    # def __init__(self, capacity: int):
+    #     self.cache = dict()
+    #     self.dq = deque()
+    #     self.capacity = capacity
+        
+    # Using Linked List
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
